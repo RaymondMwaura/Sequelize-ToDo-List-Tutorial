a34dfbd7600462cb77f2281609d8c6b780bd6a86
RaymondMwaura <raywachaga@gmail.com>
2019-11-12 19:51:32 +0200

    Sequelize ToDo List Tutorial

---
+++ bin/www.js
@@ -0,0 +1,9 @@
+// Acts as some sort of base server
+const http = require('http');
+const app = require('../app');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
